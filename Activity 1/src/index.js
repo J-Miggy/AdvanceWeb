092f15d5b2ce735985bc6dcb643a41d95e09495a
J-Miggy <jose.parami@lsu.edu.ph>
2022-02-17 21:40:46 +0800

    second commit

--- Activity 1/src/index.js
+++ Activity 1/src/index.js
@@ -1,6 +1,10 @@
 import "./styles.css";
 
 document.getElementById("app").innerHTML = `
-<h1>#LetLeniLeave<br>-Jose Miguel</br></h1>
-
+<h1>#LetLeniLeave</h1>
+<br>
+<button>Agree</button>
+<button>Disagree</button>
+<br>
+<p>If you select "Disagree" you need to educate yourself with #KaKamPinks</p>
 `;
